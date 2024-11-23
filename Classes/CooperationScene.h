5fafc834c15cc0229155710b9b1fcd4470fc4515
韦瑾钰 <1057049426@qq.com>
2024-11-23 18:56:47 +0800

    update coop

--- Classes/CooperationScene.h
+++ Classes/CooperationScene.h
@@ -5,14 +5,21 @@
 
 class Cooperation : public cocos2d::Scene {
 public:
-	static cocos2d::Scene* createScene();
-	
-	virtual bool init();
+    static cocos2d::Scene* createScene();
 
-	// 实现静态 Cooperation* create()函数
-	CREATE_FUNC(Cooperation);
+    virtual bool init();
 
-	// 返回项的回调函数
-	void backCallBack(cocos2d::Ref* pSender);
+    // 实现静态 Cooperation* create()函数
+    CREATE_FUNC(Cooperation);
+
+private:
+    // 返回项的回调函数
+    void backCallBack(cocos2d::Ref* pSender);
+    void onUpButtonClicked(cocos2d::Ref* pSender);
+    void onDownButtonClicked(cocos2d::Ref* pSender);
+
+    // 存储 coopText 的指针
+    cocos2d::Label* _coopText;
 };
-#endif
+
+#endif // __COOPERATION_SCENE_H__
